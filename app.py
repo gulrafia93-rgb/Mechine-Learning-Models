922a8423828ff59de0b7b2c11d36468c371d9a4b
Rafia Gul <gulrafia93@gmail.com>
2026-05-31 19:00:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-# ── App create karo ──────────────────────────
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-# ── Model load karo (sirf ek baar) ───────────
-print("Model load ho raha hai...")
+
+print("Model loading...")
 model = load_model('final_model.keras')
 print("✅ Model ready!")
 
-# ── Image predict karne ka function ──────────
+
 def predict_image(img_path):
     # Image open karo aur resize karo
     img = image.load_img(img_path, target_size=(150, 150))
@@ -40,7 +40,7 @@ def predict_image(img_path):
 
     return label, confidence
 
-# ── Routes ───────────────────────────────────
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -49,12 +49,12 @@ def home():
 def predict():
     # File check karo
     if 'file' not in request.files:
-        return jsonify({'error': 'Koi file nahi mili!'})
+        return jsonify({'error': 'no file found!'})
 
     file = request.files['file']
 
     if file.filename == '':
-        return jsonify({'error': 'File select nahi ki!'})
+        return jsonify({'error': 'File not selected!'})
 
     # File save karo
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
@@ -69,7 +69,7 @@ def predict():
         'confidence': confidence
     })
 
-# ── Start Server
+
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
     app.run(debug=True, port=5000)
